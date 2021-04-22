5508df8e876e73e3be5939bc10bb79d154c103f3
mateoferrari97 <mateo.ferrari97@gmail.com>
2021-04-22 12:37:56 -0300

    Add storage GetStudentSubjects tests

---
+++ cmd/server/internal/service/storage/storage_test.go
@@ -0,0 +1,242 @@
+package storage
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStorage_GetStudentSubjects(t *testing.T) {
+	// Given
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("could not start sql mock: %v", err)
+	}
+
+	defer db.Close()
+
+	storage_ := NewStorage(sqlx.NewDb(db, ""))
+
+	q := `SELECT cs.subject_id,
+       s.name,
+       cs.correlative_id,
+       cs.type,
+       scs.status,
+       scs.description
+FROM student AS st
+         INNER JOIN student_career sc ON sc.student_id = st.id
+         INNER JOIN career_subject cs ON cs.career_id = sc.career_id
+         INNER JOIN career c ON c.id = cs.career_id
+         INNER JOIN student_career_subject scs ON scs.career_subject_id = cs.id AND st.id = scs.student_id
+         INNER JOIN subject s on s.id = cs.subject_id
+WHERE st.email = ? AND c.id = ?`
+	mock.ExpectPrepare(q).WillReturnError(nil)
+	mock.ExpectQuery(q).
+		WithArgs("example@gmail.com", "1").
+		WillReturnError(nil).
+		WillReturnRows(
+			sqlmock.NewRows([]string{"subject_id", "name", "correlative_id", "type", "status", "description"}).
+				AddRow(2, "Subject 2", nil, "REQUIRED", "PENDING", nil))
+
+	// When
+	subjects, err := storage_.GetStudentSubjects("example@gmail.com", "1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Then
+	require.NotNil(t, subjects)
+	require.Len(t, subjects, 1)
+
+	for _, subject := range subjects {
+		require.Equal(t, 2, subject.ID)
+		require.Equal(t, "Subject 2", subject.Name)
+		require.Equal(t, 0, subject.CorrelativeID)
+		require.Equal(t, "REQUIRED", subject.Type)
+		require.Equal(t, "PENDING", subject.Status)
+		require.Nil(t, subject.Description)
+	}
+}
+
+func TestStorage_GetStudentSubjects_SubjectHasCorrelative(t *testing.T) {
+	// Given
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("could not start sql mock: %v", err)
+	}
+
+	defer db.Close()
+
+	storage_ := NewStorage(sqlx.NewDb(db, ""))
+
+	q := `SELECT cs.subject_id,
+       s.name,
+       cs.correlative_id,
+       cs.type,
+       scs.status,
+       scs.description
+FROM student AS st
+         INNER JOIN student_career sc ON sc.student_id = st.id
+         INNER JOIN career_subject cs ON cs.career_id = sc.career_id
+         INNER JOIN career c ON c.id = cs.career_id
+         INNER JOIN student_career_subject scs ON scs.career_subject_id = cs.id AND st.id = scs.student_id
+         INNER JOIN subject s on s.id = cs.subject_id
+WHERE st.email = ? AND c.id = ?`
+	mock.ExpectPrepare(q).WillReturnError(nil)
+	mock.ExpectQuery(q).
+		WithArgs("example@gmail.com", "1").
+		WillReturnError(nil).
+		WillReturnRows(
+			sqlmock.NewRows([]string{"subject_id", "name", "correlative_id", "type", "status", "description"}).
+				AddRow(2, "Subject 2", 1, "REQUIRED", "PENDING", nil))
+
+	// When
+	subjects, err := storage_.GetStudentSubjects("example@gmail.com", "1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Then
+	require.NotNil(t, subjects)
+	require.Len(t, subjects, 1)
+
+	for _, subject := range subjects {
+		require.Equal(t, 2, subject.ID)
+		require.Equal(t, "Subject 2", subject.Name)
+		require.Equal(t, 1, subject.CorrelativeID)
+		require.Equal(t, "REQUIRED", subject.Type)
+		require.Equal(t, "PENDING", subject.Status)
+		require.Nil(t, subject.Description)
+	}
+}
+
+func TestStorage_GetStudentSubjects_SubjectHasDescription(t *testing.T) {
+	// Given
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("could not start sql mock: %v", err)
+	}
+
+	defer db.Close()
+
+	storage_ := NewStorage(sqlx.NewDb(db, ""))
+
+	q := `SELECT cs.subject_id,
+       s.name,
+       cs.correlative_id,
+       cs.type,
+       scs.status,
+       scs.description
+FROM student AS st
+         INNER JOIN student_career sc ON sc.student_id = st.id
+         INNER JOIN career_subject cs ON cs.career_id = sc.career_id
+         INNER JOIN career c ON c.id = cs.career_id
+         INNER JOIN student_career_subject scs ON scs.career_subject_id = cs.id AND st.id = scs.student_id
+         INNER JOIN subject s on s.id = cs.subject_id
+WHERE st.email = ? AND c.id = ?`
+	mock.ExpectPrepare(q).WillReturnError(nil)
+	mock.ExpectQuery(q).
+		WithArgs("example@gmail.com", "1").
+		WillReturnError(nil).
+		WillReturnRows(
+			sqlmock.NewRows([]string{"subject_id", "name", "correlative_id", "type", "status", "description"}).
+				AddRow(2, "Subject 2", nil, "REQUIRED", "PENDING", "..."))
+
+	// When
+	subjects, err := storage_.GetStudentSubjects("example@gmail.com", "1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Then
+	require.NotNil(t, subjects)
+	require.Len(t, subjects, 1)
+
+	for _, subject := range subjects {
+		require.Equal(t, 2, subject.ID)
+		require.Equal(t, "Subject 2", subject.Name)
+		require.Equal(t, 0, subject.CorrelativeID)
+		require.Equal(t, "REQUIRED", subject.Type)
+		require.Equal(t, "PENDING", subject.Status)
+		require.NotNil(t, subject.Description)
+		require.Equal(t, "...", *subject.Description)
+	}
+}
+
+func TestStorage_GetStudentSubjects_PrepareStmtError(t *testing.T) {
+	// Given
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("could not start sql mock: %v", err)
+	}
+
+	defer db.Close()
+
+	storage_ := NewStorage(sqlx.NewDb(db, ""))
+
+	q := `SELECT cs.subject_id,
+       s.name,
+       cs.correlative_id,
+       cs.type,
+       scs.status,
+       scs.description
+FROM student AS st
+         INNER JOIN student_career sc ON sc.student_id = st.id
+         INNER JOIN career_subject cs ON cs.career_id = sc.career_id
+         INNER JOIN career c ON c.id = cs.career_id
+         INNER JOIN student_career_subject scs ON scs.career_subject_id = cs.id AND st.id = scs.student_id
+         INNER JOIN subject s on s.id = cs.subject_id
+WHERE st.email = ? AND c.id = ?`
+	mock.ExpectPrepare(q).WillReturnError(errors.New("error"))
+
+	// When
+	_, err = storage_.GetStudentSubjects("example@gmail.com", "1")
+	if err == nil {
+		t.Fatal("test must fail")
+	}
+
+	// Then
+	require.EqualError(t, err, "error")
+}
+
+func TestStorage_GetStudentSubjects_ExecuteStmtError(t *testing.T) {
+	// Given
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("could not start sql mock: %v", err)
+	}
+
+	defer db.Close()
+
+	storage_ := NewStorage(sqlx.NewDb(db, ""))
+
+	q := `SELECT cs.subject_id,
+       s.name,
+       cs.correlative_id,
+       cs.type,
+       scs.status,
+       scs.description
+FROM student AS st
+         INNER JOIN student_career sc ON sc.student_id = st.id
+         INNER JOIN career_subject cs ON cs.career_id = sc.career_id
+         INNER JOIN career c ON c.id = cs.career_id
+         INNER JOIN student_career_subject scs ON scs.career_subject_id = cs.id AND st.id = scs.student_id
+         INNER JOIN subject s on s.id = cs.subject_id
+WHERE st.email = ? AND c.id = ?`
+	mock.ExpectPrepare(q).WillReturnError(nil)
+	mock.ExpectQuery(q).
+		WithArgs("example@gmail.com", "1").
+		WillReturnError(errors.New("error"))
+
+	// When
+	_, err = storage_.GetStudentSubjects("example@gmail.com", "1")
+	if err == nil {
+		t.Fatal("test must fail")
+	}
+
+	// Then
+	require.EqualError(t, err, "error")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mateoferrari97/my-path
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.3
